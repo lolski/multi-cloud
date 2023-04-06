c6e7ab4939732550c99d90a4c38e3435b403905a
Ganeshwara Hananda <ganesh@vaticle.com>
2023-04-06 15:28:25 +0100

    refactor

--- main.tf
+++ main.tf
@@ -1,7 +1,15 @@
+//////////////////////////////
+// config
+//////////////////////////////
+
 locals {
   cluster = "ganesh"
   gcp_project_number = "225200396825" // vaticle-typedb-cloud-dev
 }
+
+//////////////////////////////
+// terraform providers
+//////////////////////////////
 
 terraform {
   required_providers {
@@ -12,19 +20,21 @@ terraform {
   }
 }
 
-//
+//////////////////////////////
 // declare AWS cluster
-//
+//////////////////////////////
 
 // provider
 provider "aws" {
   region = "eu-west-2"
 }
 
+
 // vpc
 resource "aws_vpc" "net" {
   cidr_block = "10.0.0.0/16"
 }
+
 
 // kms keys
 resource "aws_kms_key" "kms-key" {
@@ -35,6 +45,7 @@ resource "aws_kms_alias" "kms-key-alias" {
   target_key_id = aws_kms_key.kms-key.arn
   name = "alias/${local.cluster}-kms-key"
 }
+
 
 // roles
 data "aws_iam_policy_document" "policy-multi-cloud-api" {
@@ -91,11 +102,3 @@ resource "aws_iam_role" "role-node-pool" {
   name = "${local.cluster}-role-node-pool"
   assume_role_policy = data.aws_iam_policy_document.policy-node-pool.json
 }
-
-//
-// declare GCP cluster
-//
-
-//
-// declare fleet (in GCP)
-//
