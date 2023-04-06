87f9a403c33c8f94142cdf20f7c3c2b08fc88791
Ganeshwara Hananda <ganesh@vaticle.com>
2023-04-06 11:38:48 +0100

    configure KMS

--- main.tf
+++ main.tf
@@ -7,15 +7,34 @@ terraform {
   }
 }
 
+//
 // declare AWS cluster
+//
+
+// provider
 provider "aws" {
   region = "eu-west-2"
 }
 
+// vpc
 resource "aws_vpc" "ganesh-net" {
   cidr_block = "10.0.0.0/16"
 }
 
-// declare GCP cluster
+// kms keys
+resource "aws_kms_key" "ganesh-kms-key" {
+  description = "KMS key used by Kubernetes"
+}
 
+resource "aws_kms_alias" "ganesh-kms-key-alias" {
+  target_key_id = aws_kms_key.ganesh-kms-key.arn
+  name = "alias/ganesh-kms-key"
+}
+
+//
+// declare GCP cluster
+//
+
+//
 // declare fleet (in GCP)
+//
